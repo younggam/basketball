67236bc84333d04d654c356e46be4b64396e3335
younggam <nainjoo2001@naver.com>
2025-11-26 16:11:58 +0900

    add collision bounce by goal

--- basketball.srcs/sources_1/new/game.v
+++ basketball.srcs/sources_1/new/game.v
@@ -38,11 +38,12 @@ module game (
     localparam GOAL_X_LEFT=20;
     localparam GOAL_X_RIGHT=100;
     localparam GOAL_Y=100;
-    localparam BALL_SIZE=20;
+    localparam BALL_RADIUS=12;
     localparam BALL_START_X=760;
     localparam BALL_START_Y=380;
 
     reg [1:0] state, next_state;
+    reg [15:0] score;
     
     // 물리 변수 (속도, 중력 등)
     reg signed [9:0] velocity_x, velocity_y;
@@ -52,22 +53,36 @@ module game (
     always @(posedge clk or negedge resetn) begin
         if (!resetn) begin
             state <= IDLE;
-            // 초기 위치 리셋
+            ball_x <= BALL_START_X;
+            ball_y <= BALL_START_Y;
         end else begin
             state <= next_state;
             // 상태에 따른 좌표 업데이트 로직
             case (state)
+                // 초기 위치 고정
                 IDLE: begin
                     ball_x <= BALL_START_X;
                     ball_y <= BALL_START_Y;
                 end
-                FLYING: begin
+                FLYING, GOAL: begin
+                    // 골대와 충돌 시 반발
+                    if (ball_y - GOAL_Y <= BALL_RADIUS && GOAL_Y - ball_y <= BALL_RADIUS) begin // Y 검사
+                        if (ball_x - GOAL_X_LEFT <= BALL_RADIUS && GOAL_X_LEFT - ball_x <= BALL_RADIUS) begin // 골대 왼쪽 끝과 충돌
+                            if (ball_x - GOAL_X_LEFT >= ball_y - GOAL_Y  
+                                || GOAL_X_LEFT - ball_x >= ball_y - GOAL_Y) velocity_x <= -velocity_x; // 좌 우 에서 접근 판별
+                            else velocity_y <= -velocity_y; // 아래 위 에서 접근 판별
+                        end
+                        else if (ball_x - GOAL_X_RIGHT <= BALL_RADIUS && GOAL_X_RIGHT - ball_x <= BALL_RADIUS) begin // 골대 오른쪽 끝과 충돌
+                            if (ball_x - GOAL_X_RIGHT >= ball_y - GOAL_Y
+                                || GOAL_X_RIGHT - ball_x >= ball_y - GOAL_Y) velocity_x <= -velocity_x; // 좌 우 에서 접근 판별
+                            else velocity_y <= -velocity_y; // 아래 위에서 접근 판별
+                        end
+                    end
                     ball_x <= ball_x + velocity_x;
-                    ball_y <= ball_y + velocity_y; // 화면 좌표계는 아래가 +Y일 수 있음 유의
+                    ball_y <= ball_y + velocity_y;
                     velocity_y <= velocity_y + GRAVITY; // 중력 적용 (포물선 운동)
                 end
             endcase
-            // ... (초기화 및 기타 로직)
         end
     end
 
@@ -81,18 +96,20 @@ module game (
             FLYING: begin
                 // 바닥에 닿거나 골대에 닿으면 상태 변경
                 if (ball_y >= 460) next_state = IDLE; // 바닥 닿음
-                else if (ball_y==GOAL_Y) begin
-                    if (ball_x > GOAL_X_LEFT && ball_x < GOAL_X_RIGHT) next_state = GOAL;
-                    else begin
-                        if (ball_x == GOAL_X_LEFT || ball_x == GOAL_X_RIGHT) begin
-                            
-                        end 
-                        next_state = FLYING;
-                    end
+                else if (ball_y - GOAL_Y <= BALL_RADIUS && GOAL_Y - ball_y <= BALL_RADIUS
+                    && ball_x - GOAL_X_LEFT > BALL_RADIUS && GOAL_X_RIGHT - ball_x > BALL_RADIUS
+                    && velocity_y <= 0) begin
+                    score = score + 1; // 골 점수
+                    velocity_x = velocity_x >>> 1; // 골망 효과 속도 감소
+                    velocity_y = velocity_y >>> 1;
+                    next_state = GOAL;
                 end
                 else next_state = FLYING;
             end
-            // ...
+            GOAL: begin
+                if (ball_y >= 460) next_state = IDLE; // 바닥 닿음
+                else next_state = GOAL;
+            end
         endcase
     end
 
