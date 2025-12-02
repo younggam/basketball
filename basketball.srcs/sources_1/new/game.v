04fb1d5f819e5f50350e6e0baef8421e7c1e5b2b
younggam <nainjoo2001@naver.com>
2025-12-02 17:30:47 +0900

    sequential logic is bug spawner at game logic

--- basketball.srcs/sources_1/new/game.v
+++ basketball.srcs/sources_1/new/game.v
@@ -54,7 +54,7 @@ module game
     
     // 물리 변수 (속도, 중력 등)
     reg signed [9:0] velocity_x;
-    reg signed [9:0]velocity_y;
+    reg signed [9:0] velocity_y;
     
     reg [1:0] cnt_200ms;
     reg tick_200ms;
@@ -88,13 +88,13 @@ module game
     // 1. 상태 전이 로직 (Sequential)
     always @(posedge clk or negedge resetn) begin
         if (!resetn) begin
-            power <= 0;
+            power = 0;
             state = IDLE;
-            score <= 0;
-            ball_x <= PLAYER_X;
-            ball_y <= PLAYER_Y;
-            velocity_x <= 0;
-            velocity_y <= 0;
+            score = 0;
+            ball_x = PLAYER_X;
+            ball_y = PLAYER_Y;
+            velocity_x = 0;
+            velocity_y = 0;
         end
         else begin
             if(tick_50ms) begin
@@ -103,20 +103,20 @@ module game
                     IDLE: begin
                         if (btn_throw) begin
                             if (power == 0) begin
-                                empowering <= 1;
-                                power <= 1;
+                                empowering = 1;
+                                power = 1;
                             end
                             else if (tick_200ms) begin
-                                if (power == 4'b1111) empowering <= 0;
-                                else if (power == 1) empowering <= 1;
-                                if (empowering) power <= power + 1;
-                                else power <= power - 1;
+                                if (power == 4'b1111) empowering = 0;
+                                else if (power == 1) empowering = 1;
+                                if (empowering) power = power + 1;
+                                else power = power - 1;
                             end
                         end                
                         else if (power > 0) begin
-                            velocity_x <= power * -sw_speed_x;
-                            velocity_y <= power * -sw_speed_y;
-                            power <= 0;
+                            velocity_x = power * -sw_speed_x;
+                            velocity_y = power * -sw_speed_y;
+                            power = 0;
                             state = FLYING;
                         end
                         else state = IDLE;
@@ -125,9 +125,9 @@ module game
                         // 바닥에 닿거나 골대에 닿으면 상태 변경
                         if (ball_y >= 480 - BALL_RADIUS || ball_x + BALL_RADIUS < 0 ) state = IDLE; // 공 넘어감
                         else if (collide_line(ball_x, ball_y, BALL_RADIUS, GOAL_X_LEFT, GOAL_X_RIGHT, GOAL_Y) && velocity_y <= 0) begin
-                            score <= score + 1; // 골 점수
-                            velocity_x <= velocity_x >>> 1; // 골망 효과 속도 감소
-                            velocity_y <= velocity_y >>> 1;
+                            score = score + 1; // 골 점수
+                            velocity_x = velocity_x >>> 1; // 골망 효과 속도 감소
+                            velocity_y = velocity_y >>> 1;
                             state = GOAL;
                         end
                         else state = FLYING;
@@ -142,26 +142,26 @@ module game
                 case (state)
                     // 초기 위치 고정
                     IDLE: begin
-                        ball_x <= PLAYER_X;
-                        ball_y <= PLAYER_Y;
-                        velocity_x <= 0;
-                        velocity_y <= 0;
+                        ball_x = PLAYER_X;
+                        ball_y = PLAYER_Y;
+                        velocity_x = 0;
+                        velocity_y = 0;
                     end
                     FLYING, GOAL: begin
                         // 골대와 충돌 시 반발
                         if (collide_dot(ball_x,ball_y,BALL_RADIUS,GOAL_X_LEFT,GOAL_Y)) begin // 골대 왼쪽 끝과 충돌
                             if (ball_x - GOAL_X_LEFT >= ball_y - GOAL_Y && GOAL_X_LEFT - ball_x <= ball_y - GOAL_Y
-                            || ball_x - GOAL_X_LEFT <= ball_y - GOAL_Y && GOAL_X_LEFT - ball_x >= ball_y - GOAL_Y) velocity_x <= -velocity_x; // 좌 우 에서 접근 판별
-                            else velocity_y <= -velocity_y; // 아래 위 에서 접근 판별
+                            || ball_x - GOAL_X_LEFT <= ball_y - GOAL_Y && GOAL_X_LEFT - ball_x >= ball_y - GOAL_Y) velocity_x = -velocity_x; // 좌 우 에서 접근 판별
+                            else velocity_y = -velocity_y; // 아래 위 에서 접근 판별
                         end
                         else if (collide_dot(ball_x,ball_y,BALL_RADIUS,GOAL_X_RIGHT,GOAL_Y)) begin // 골대 오른쪽 끝과 충돌
                             if (ball_x - GOAL_X_RIGHT >= ball_y - GOAL_Y && GOAL_X_RIGHT - ball_x <= ball_y - GOAL_Y
-                            || ball_x - GOAL_X_RIGHT <= ball_y - GOAL_Y && GOAL_X_RIGHT - ball_x >= ball_y - GOAL_Y) velocity_x <= -velocity_x; // 좌 우 에서 접근 판별
-                            else velocity_y <= -velocity_y; // 아래 위에서 접근 판별
+                            || ball_x - GOAL_X_RIGHT <= ball_y - GOAL_Y && GOAL_X_RIGHT - ball_x >= ball_y - GOAL_Y) velocity_x = -velocity_x; // 좌 우 에서 접근 판별
+                            else velocity_y = -velocity_y; // 아래 위에서 접근 판별
                         end
-                        ball_x <= ball_x + velocity_x;
-                        ball_y <= ball_y + velocity_y;
-                        velocity_y <= velocity_y + GRAVITY; // 중력 적용 (포물선 운동)
+                        ball_x = ball_x + velocity_x;
+                        ball_y = ball_y + velocity_y;
+                        velocity_y = velocity_y + GRAVITY; // 중력 적용 (포물선 운동)
                     end
                 endcase
             end
